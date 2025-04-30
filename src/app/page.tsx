7f1701e2f03ee47a02543bff49d26be611ddd40b
GrmsSan <gugubriel15@gmail.com>
2025-04-30 16:54:40 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import image from 'next/image'
+
+export default function home (){
+  return(
+    <>
+
+    <div className="before:content-['hello\_world']">
+
+    <h1 className="absolute top-0 left-0 size-16"></h1>
+
+    <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Gustavo</h3>
+
+    </div>
+
+   </>
+  )
+
+
+}
